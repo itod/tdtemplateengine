8f99501ac52cbdc41b2a9b07a7539f33a85b4d47
Todd Ditchendorf <todd.ditchendorf@gmail.com>
2014-04-04 16:38:18 -0500

    * neg tests

--- test/XPNegationExpressionTests.m
+++ test/XPNegationExpressionTests.m
@@ -407,31 +407,9 @@
     TDNotNil(expr);
     TDFalse([[expr simplify] evaluateAsBooleanInContext:nil]);
 }
-//
-//- (void)testBangOpen1EqSign1Close {
-//    NSString *input = @"!(1 = 1)";
-//    NSArray *toks = [self tokenize:input];
-//    
-//    NSError *err = nil;
-//    XPExpression *expr = [XPExpression expressionFromTokens:toks error:&err];
-//    TDNil(err);
-//    TDNotNil(expr);
-//    TDFalse([[expr simplify] evaluateAsBooleanInContext:nil]);
-//}
 
-- (void)test1EqEqSign1 {
-    NSString *input = @"1 == 1";
-    NSArray *toks = [self tokenize:input];
-    
-    NSError *err = nil;
-    XPExpression *expr = [XPExpression expressionFromTokens:toks error:&err];
-    TDNil(err);
-    TDNotNil(expr);
-    TDTrue([[expr simplify] evaluateAsBooleanInContext:nil]);
-}
-
-- (void)test1Eq2 {
-    NSString *input = @"1 eq 2";
+- (void)testBangOpen1EqSign1Close {
+    NSString *input = @"!(1 = 1)";
     NSArray *toks = [self tokenize:input];
     
     NSError *err = nil;
@@ -441,8 +419,8 @@
     TDFalse([[expr simplify] evaluateAsBooleanInContext:nil]);
 }
 
-- (void)test1EqSign2 {
-    NSString *input = @"1 = 2";
+- (void)testBang1EqEqSign1 {
+    NSString *input = @"!1 == 1";
     NSArray *toks = [self tokenize:input];
     
     NSError *err = nil;
@@ -452,8 +430,8 @@
     TDFalse([[expr simplify] evaluateAsBooleanInContext:nil]);
 }
 
-- (void)test1EqEqSign2 {
-    NSString *input = @"1 == 2";
+- (void)test1EqNot1 {
+    NSString *input = @"1 eq not 1";
     NSArray *toks = [self tokenize:input];
     
     NSError *err = nil;
@@ -463,8 +441,8 @@
     TDFalse([[expr simplify] evaluateAsBooleanInContext:nil]);
 }
 
-- (void)test1Ne1 {
-    NSString *input = @"1 ne 1";
+- (void)test1EqBangSpace1 {
+    NSString *input = @"1 eq ! 1";
     NSArray *toks = [self tokenize:input];
     
     NSError *err = nil;
@@ -474,8 +452,8 @@
     TDFalse([[expr simplify] evaluateAsBooleanInContext:nil]);
 }
 
-- (void)test1NeSign1 {
-    NSString *input = @"1 != 1";
+- (void)test1EqSpace1 {
+    NSString *input = @"1 eq !1";
     NSArray *toks = [self tokenize:input];
     
     NSError *err = nil;
@@ -483,358 +461,6 @@
     TDNil(err);
     TDNotNil(expr);
     TDFalse([[expr simplify] evaluateAsBooleanInContext:nil]);
-}
-
-- (void)test1Ne2 {
-    NSString *input = @"1 ne 2";
-    NSArray *toks = [self tokenize:input];
-    
-    NSError *err = nil;
-    XPExpression *expr = [XPExpression expressionFromTokens:toks error:&err];
-    TDNil(err);
-    TDNotNil(expr);
-    TDTrue([[expr simplify] evaluateAsBooleanInContext:nil]);
-}
-
-- (void)test1NeSign2 {
-    NSString *input = @"1 != 2";
-    NSArray *toks = [self tokenize:input];
-    
-    NSError *err = nil;
-    XPExpression *expr = [XPExpression expressionFromTokens:toks error:&err];
-    TDNil(err);
-    TDNotNil(expr);
-    TDTrue([[expr simplify] evaluateAsBooleanInContext:nil]);
-}
-
-- (void)test1Lt1 {
-    NSString *input = @"1 < 1";
-    NSArray *toks = [self tokenize:input];
-    
-    NSError *err = nil;
-    XPExpression *expr = [XPExpression expressionFromTokens:toks error:&err];
-    TDNil(err);
-    TDNotNil(expr);
-    TDFalse([[expr simplify] evaluateAsBooleanInContext:nil]);
-}
-
-- (void)test1LtEq1 {
-    NSString *input = @"1 <= 1";
-    NSArray *toks = [self tokenize:input];
-    
-    NSError *err = nil;
-    XPExpression *expr = [XPExpression expressionFromTokens:toks error:&err];
-    TDNil(err);
-    TDNotNil(expr);
-    TDTrue([[expr simplify] evaluateAsBooleanInContext:nil]);
-}
-
-- (void)test1Lt2 {
-    NSString *input = @"1 < 2";
-    NSArray *toks = [self tokenize:input];
-    
-    NSError *err = nil;
-    XPExpression *expr = [XPExpression expressionFromTokens:toks error:&err];
-    TDNil(err);
-    TDNotNil(expr);
-    TDTrue([[expr simplify] evaluateAsBooleanInContext:nil]);
-}
-
-- (void)test1LtEq2 {
-    NSString *input = @"1 <= 2";
-    NSArray *toks = [self tokenize:input];
-    
-    NSError *err = nil;
-    XPExpression *expr = [XPExpression expressionFromTokens:toks error:&err];
-    TDNil(err);
-    TDNotNil(expr);
-    TDTrue([[expr simplify] evaluateAsBooleanInContext:nil]);
-}
-
-- (void)test1Gt1 {
-    NSString *input = @"1 > 1";
-    NSArray *toks = [self tokenize:input];
-    
-    NSError *err = nil;
-    XPExpression *expr = [XPExpression expressionFromTokens:toks error:&err];
-    TDNil(err);
-    TDNotNil(expr);
-    TDFalse([[expr simplify] evaluateAsBooleanInContext:nil]);
-}
-
-- (void)test1GtEq1 {
-    NSString *input = @"1 >= 1";
-    NSArray *toks = [self tokenize:input];
-    
-    NSError *err = nil;
-    XPExpression *expr = [XPExpression expressionFromTokens:toks error:&err];
-    TDNil(err);
-    TDNotNil(expr);
-    TDTrue([[expr simplify] evaluateAsBooleanInContext:nil]);
-}
-
-- (void)test1Gt2 {
-    NSString *input = @"1 > 2";
-    NSArray *toks = [self tokenize:input];
-    
-    NSError *err = nil;
-    XPExpression *expr = [XPExpression expressionFromTokens:toks error:&err];
-    TDNil(err);
-    TDNotNil(expr);
-    TDFalse([[expr simplify] evaluateAsBooleanInContext:nil]);
-}
-
-- (void)test1GtEq2 {
-    NSString *input = @"1 >= 2";
-    NSArray *toks = [self tokenize:input];
-    
-    NSError *err = nil;
-    XPExpression *expr = [XPExpression expressionFromTokens:toks error:&err];
-    TDNil(err);
-    TDNotNil(expr);
-    TDFalse([[expr simplify] evaluateAsBooleanInContext:nil]);
-}
-
-- (void)test1And0 {
-    NSString *input = @"1 and 0";
-    NSArray *toks = [self tokenize:input];
-    
-    NSError *err = nil;
-    XPExpression *expr = [XPExpression expressionFromTokens:toks error:&err];
-    TDNil(err);
-    TDNotNil(expr);
-    TDFalse([[expr simplify] evaluateAsBooleanInContext:nil]);
-}
-
-- (void)test1AmpAmp0 {
-    NSString *input = @"1 && 0";
-    NSArray *toks = [self tokenize:input];
-    
-    NSError *err = nil;
-    XPExpression *expr = [XPExpression expressionFromTokens:toks error:&err];
-    TDNil(err);
-    TDNotNil(expr);
-    TDFalse([[expr simplify] evaluateAsBooleanInContext:nil]);
-}
-
-- (void)test0And1 {
-    NSString *input = @"0 and 1";
-    NSArray *toks = [self tokenize:input];
-    
-    NSError *err = nil;
-    XPExpression *expr = [XPExpression expressionFromTokens:toks error:&err];
-    TDNil(err);
-    TDNotNil(expr);
-    TDFalse([[expr simplify] evaluateAsBooleanInContext:nil]);
-}
-
-- (void)test1And1 {
-    NSString *input = @"1 and 1";
-    NSArray *toks = [self tokenize:input];
-    
-    NSError *err = nil;
-    XPExpression *expr = [XPExpression expressionFromTokens:toks error:&err];
-    TDNil(err);
-    TDNotNil(expr);
-    TDTrue([[expr simplify] evaluateAsBooleanInContext:nil]);
-}
-
-- (void)test1AmpAmp1 {
-    NSString *input = @"1 && 1";
-    NSArray *toks = [self tokenize:input];
-    
-    NSError *err = nil;
-    XPExpression *expr = [XPExpression expressionFromTokens:toks error:&err];
-    TDNil(err);
-    TDNotNil(expr);
-    TDTrue([[expr simplify] evaluateAsBooleanInContext:nil]);
-}
-
-- (void)test1Or0 {
-    NSString *input = @"1 or 0";
-    NSArray *toks = [self tokenize:input];
-    
-    NSError *err = nil;
-    XPExpression *expr = [XPExpression expressionFromTokens:toks error:&err];
-    TDNil(err);
-    TDNotNil(expr);
-    TDTrue([[expr simplify] evaluateAsBooleanInContext:nil]);
-}
-
-- (void)test1PipePipe0 {
-    NSString *input = @"1 || 0";
-    NSArray *toks = [self tokenize:input];
-    
-    NSError *err = nil;
-    XPExpression *expr = [XPExpression expressionFromTokens:toks error:&err];
-    TDNil(err);
-    TDNotNil(expr);
-    TDTrue([[expr simplify] evaluateAsBooleanInContext:nil]);
-}
-
-- (void)test0Or1 {
-    NSString *input = @"0 or 1";
-    NSArray *toks = [self tokenize:input];
-    
-    NSError *err = nil;
-    XPExpression *expr = [XPExpression expressionFromTokens:toks error:&err];
-    TDNil(err);
-    TDNotNil(expr);
-    TDTrue([[expr simplify] evaluateAsBooleanInContext:nil]);
-}
-
-- (void)test0PipePipe1 {
-    NSString *input = @"0 || 1";
-    NSArray *toks = [self tokenize:input];
-    
-    NSError *err = nil;
-    XPExpression *expr = [XPExpression expressionFromTokens:toks error:&err];
-    TDNil(err);
-    TDNotNil(expr);
-    TDTrue([[expr simplify] evaluateAsBooleanInContext:nil]);
-}
-
-- (void)test1Or1 {
-    NSString *input = @"1 or 1";
-    NSArray *toks = [self tokenize:input];
-    
-    NSError *err = nil;
-    XPExpression *expr = [XPExpression expressionFromTokens:toks error:&err];
-    TDNil(err);
-    TDNotNil(expr);
-    TDTrue([[expr simplify] evaluateAsBooleanInContext:nil]);
-}
-
-- (void)test1SpacePlusSpace2 {
-    NSString *input = @"1 + 2";
-    NSArray *toks = [self tokenize:input];
-    
-    NSError *err = nil;
-    XPExpression *expr = [XPExpression expressionFromTokens:toks error:&err];
-    TDNil(err);
-    TDNotNil(expr);
-    TDEquals(3.0, [[expr simplify] evaluateAsNumberInContext:nil]);
-}
-
-- (void)test1Plus2 {
-    NSString *input = @"1+2";
-    NSArray *toks = [self tokenize:input];
-    
-    NSError *err = nil;
-    XPExpression *expr = [XPExpression expressionFromTokens:toks error:&err];
-    TDNil(err);
-    TDNotNil(expr);
-    TDEquals(3.0, [[expr simplify] evaluateAsNumberInContext:nil]);
-}
-
-- (void)test1SpaceMinusSpace1 {
-    NSString *input = @"1 - 1";
-    NSArray *toks = [self tokenize:input];
-    
-    NSError *err = nil;
-    XPExpression *expr = [XPExpression expressionFromTokens:toks error:&err];
-    TDNil(err);
-    TDNotNil(expr);
-    TDEquals(0.0, [[expr simplify] evaluateAsNumberInContext:nil]);
-}
-
-- (void)testTruePlus1 {
-    NSString *input = @"true + 1";
-    NSArray *toks = [self tokenize:input];
-    
-    NSError *err = nil;
-    XPExpression *expr = [XPExpression expressionFromTokens:toks error:&err];
-    TDNil(err);
-    TDNotNil(expr);
-    TDEquals(2.0, [[expr simplify] evaluateAsNumberInContext:nil]);
-}
-
-- (void)testYesPlus1 {
-    NSString *input = @"YES + 1";
-    NSArray *toks = [self tokenize:input];
-    
-    NSError *err = nil;
-    XPExpression *expr = [XPExpression expressionFromTokens:toks error:&err];
-    TDNil(err);
-    TDNotNil(expr);
-    TDEquals(2.0, [[expr simplify] evaluateAsNumberInContext:nil]);
-}
-
-- (void)testFalsePlus1 {
-    NSString *input = @"false + 1";
-    NSArray *toks = [self tokenize:input];
-    
-    NSError *err = nil;
-    XPExpression *expr = [XPExpression expressionFromTokens:toks error:&err];
-    TDNil(err);
-    TDNotNil(expr);
-    TDEquals(1.0, [[expr simplify] evaluateAsNumberInContext:nil]);
-}
-
-- (void)testNoPlus1 {
-    NSString *input = @"NO + 1";
-    NSArray *toks = [self tokenize:input];
-    
-    NSError *err = nil;
-    XPExpression *expr = [XPExpression expressionFromTokens:toks error:&err];
-    TDNil(err);
-    TDNotNil(expr);
-    TDEquals(1.0, [[expr simplify] evaluateAsNumberInContext:nil]);
-}
-
-- (void)test2Times2 {
-    NSString *input = @"2*2";
-    NSArray *toks = [self tokenize:input];
-    
-    NSError *err = nil;
-    XPExpression *expr = [XPExpression expressionFromTokens:toks error:&err];
-    TDNil(err);
-    TDNotNil(expr);
-    TDEquals(4.0, [[expr simplify] evaluateAsNumberInContext:nil]);
-}
-
-- (void)test2Div2 {
-    NSString *input = @"2/2";
-    NSArray *toks = [self tokenize:input];
-    
-    NSError *err = nil;
-    XPExpression *expr = [XPExpression expressionFromTokens:toks error:&err];
-    TDNil(err);
-    TDNotNil(expr);
-    TDEquals(1.0, [[expr simplify] evaluateAsNumberInContext:nil]);
-}
-
-- (void)test3Plus2Times2 {
-    NSString *input = @"3+2*2";
-    NSArray *toks = [self tokenize:input];
-    
-    NSError *err = nil;
-    XPExpression *expr = [XPExpression expressionFromTokens:toks error:&err];
-    TDNil(err);
-    TDNotNil(expr);
-    TDEquals(7.0, [[expr simplify] evaluateAsNumberInContext:nil]);
-}
-
-- (void)testOpen3Plus2CloseTimes2 {
-    NSString *input = @"(3+2)*2";
-    NSArray *toks = [self tokenize:input];
-    
-    NSError *err = nil;
-    XPExpression *expr = [XPExpression expressionFromTokens:toks error:&err];
-    TDNil(err);
-    TDNotNil(expr);
-    TDEquals(10.0, [[expr simplify] evaluateAsNumberInContext:nil]);
-}
-
-- (void)testMinus1 {
-    NSString *input = @"-1";
-    NSArray *toks = [self tokenize:input];
-    
-    NSError *err = nil;
-    XPExpression *expr = [XPExpression expressionFromTokens:toks error:&err];
-    TDNil(err);
-    TDNotNil(expr);
-    TDEquals(-1.0, [[expr simplify] evaluateAsNumberInContext:nil]);
 }
 
 @end
