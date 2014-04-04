9845c0b85dba142f8834e5939ba848c902cde70e
Todd Ditchendorf <todd.ditchendorf@gmail.com>
2014-04-04 14:38:36 -0500

    * range tests

--- test/XPRangeExpressionTests.m
+++ test/XPRangeExpressionTests.m
@@ -66,12 +66,51 @@
     }
 }
 
-
 - (void)testIIn5To2 {
     NSString *input = @"i in 5 to 2";
     NSArray *toks = [self tokenize:input];
     
     id foo = @[@(5), @(4), @(3), @(2)];
+    id vars = @{@"foo": foo};
+    TDTemplateContext *ctx = [[[TDTemplateContext alloc] initWithVariables:vars output:nil] autorelease];
+    
+    NSError *err = nil;
+    XPExpression *expr = [[XPExpression expressionFromTokens:toks error:&err] simplify];
+    TDNil(err);
+    TDNotNil(expr);
+    TDTrue([expr isKindOfClass:[XPLoopExpression class]]);
+    
+    for (id obj in foo) {
+        id res = [expr evaluateInContext:ctx];
+        TDEqualObjects(obj, res);
+    }
+}
+
+- (void)testIIn0To10By2 {
+    NSString *input = @"i in 0 to 10 by 2";
+    NSArray *toks = [self tokenize:input];
+    
+    id foo = @[@(0), @(2), @(4), @(6), @(8), @(10)];
+    id vars = @{@"foo": foo};
+    TDTemplateContext *ctx = [[[TDTemplateContext alloc] initWithVariables:vars output:nil] autorelease];
+    
+    NSError *err = nil;
+    XPExpression *expr = [[XPExpression expressionFromTokens:toks error:&err] simplify];
+    TDNil(err);
+    TDNotNil(expr);
+    TDTrue([expr isKindOfClass:[XPLoopExpression class]]);
+    
+    for (id obj in foo) {
+        id res = [expr evaluateInContext:ctx];
+        TDEqualObjects(obj, res);
+    }
+}
+
+- (void)testIIn10To0ByNeg2 {
+    NSString *input = @"i in 10 to 0 by -2";
+    NSArray *toks = [self tokenize:input];
+    
+    id foo = @[@(10), @(8), @(6), @(4), @(2), @(0)];
     id vars = @{@"foo": foo};
     TDTemplateContext *ctx = [[[TDTemplateContext alloc] initWithVariables:vars output:nil] autorelease];
     
