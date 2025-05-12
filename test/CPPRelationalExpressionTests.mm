624bd6c9688c6701cd19b6f6e40d754b1da57013
Todd Ditchendorf <todd.ditchendorf@gmail.com>
2025-05-12 18:56:48 -0500

    * tests

---
+++ test/CPPRelationalExpressionTests.mm
@@ -0,0 +1,131 @@
+//
+//  CPPRelationalExpressionTests.m
+//  TDTemplateEngineTests
+//
+//  Created by Todd Ditchendorf on 3/28/14.
+//  Copyright (c) 2014 Todd Ditchendorf. All rights reserved.
+//
+
+#import "TDBaseExpressionTests.h"
+#import "ExpressionParser.hpp"
+
+using namespace parsekit;
+using namespace templateengine;
+
+@interface CPPRelationalExpressionTests : TDBaseExpressionTests
+
+@end
+
+@implementation CPPRelationalExpressionTests
+
+- (void)setUp {
+    [super setUp];
+    
+}
+
+- (void)tearDown {
+    
+    [super tearDown];
+}
+
+- (void)test1Lt1 {
+    std::string input = "1 < 1";
+
+    ExpressionParser p;
+    Reader r(input);
+    TDExpression *expr = p.parse(&r);
+    XCTAssertNotNil(expr);
+    XCTAssertFalse([[expr simplify] evaluateAsBooleanInContext:nil]);
+}
+
+- (void)test1LtEq1 {
+    std::string input = "1 <= 1";
+
+    ExpressionParser p;
+    Reader r(input);
+    TDExpression *expr = p.parse(&r);
+    XCTAssertNotNil(expr);
+    XCTAssertTrue([[expr simplify] evaluateAsBooleanInContext:nil]);
+}
+
+- (void)test1Lt2 {
+    std::string input = "1 < 2";
+
+    ExpressionParser p;
+    Reader r(input);
+    TDExpression *expr = p.parse(&r);
+    XCTAssertNotNil(expr);
+    XCTAssertTrue([[expr simplify] evaluateAsBooleanInContext:nil]);
+}
+
+- (void)test1LtEq2 {
+    std::string input = "1 <= 2";
+
+    ExpressionParser p;
+    Reader r(input);
+    TDExpression *expr = p.parse(&r);
+    XCTAssertNotNil(expr);
+    XCTAssertTrue([[expr simplify] evaluateAsBooleanInContext:nil]);
+}
+
+- (void)test1Gt1 {
+    std::string input = "1 > 1";
+
+    ExpressionParser p;
+    Reader r(input);
+    TDExpression *expr = p.parse(&r);
+    XCTAssertNotNil(expr);
+    XCTAssertFalse([[expr simplify] evaluateAsBooleanInContext:nil]);
+}
+
+- (void)test1GtEq1 {
+    std::string input = "1 >= 1";
+
+    ExpressionParser p;
+    Reader r(input);
+    TDExpression *expr = p.parse(&r);
+    XCTAssertNotNil(expr);
+    XCTAssertTrue([[expr simplify] evaluateAsBooleanInContext:nil]);
+}
+
+- (void)test1Gt2 {
+    std::string input = "1 > 2";
+
+    ExpressionParser p;
+    Reader r(input);
+    TDExpression *expr = p.parse(&r);
+    XCTAssertNotNil(expr);
+    XCTAssertFalse([[expr simplify] evaluateAsBooleanInContext:nil]);
+}
+
+- (void)test1GtEq2 {
+    std::string input = "1 >= 2";
+
+    ExpressionParser p;
+    Reader r(input);
+    TDExpression *expr = p.parse(&r);
+    XCTAssertNotNil(expr);
+    XCTAssertFalse([[expr simplify] evaluateAsBooleanInContext:nil]);
+}
+
+- (void)testNeg0Lt0 {
+    std::string input = "-0 < 0";
+
+    ExpressionParser p;
+    Reader r(input);
+    TDExpression *expr = p.parse(&r);
+    XCTAssertNotNil(expr);
+    XCTAssertFalse([[expr simplify] evaluateAsBooleanInContext:nil]);
+}
+
+- (void)testNeg0Gt0 {
+    std::string input = "-0.0 > 0.0";
+
+    ExpressionParser p;
+    Reader r(input);
+    TDExpression *expr = p.parse(&r);
+    XCTAssertNotNil(expr);
+    XCTAssertFalse([[expr simplify] evaluateAsBooleanInContext:nil]);
+}
+
+@end
