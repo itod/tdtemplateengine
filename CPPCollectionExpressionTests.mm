054f4e680635e56848d652eb4848d61868e1ceed
Todd Ditchendorf <todd.ditchendorf@gmail.com>
2025-05-12 21:40:56 -0500

    * tests

--- CPPCollectionExpressionTests.mm
+++ CPPCollectionExpressionTests.mm
@@ -49,105 +49,105 @@ using namespace parsekit;
     TDNil([expr evaluateInContext:ctx]);
 }
 
-//- (void)testIInArrayOneTwoThree {
-//    std::string input = "i in foo";
-//    Reader reader(input);
-//    
-//    id foo = @[@"one", @"two", @"three"];
-//    id vars = @{@"foo": foo};
-//    TDTemplateContext *ctx = [[[TDTemplateContext alloc] initWithVariables:vars output:nil] autorelease];
-//    
-//    NSError *err = nil;
-//    TDExpression *expr = [[self.eng loopExpressionFromReader:&reader error:&err] simplify];
-//    TDNil(err);
-//    TDNotNil(expr);
-//    TDTrue([expr isKindOfClass:[TDLoopExpression class]]);
-//    
-//    for (id obj in foo) {
-//        id res = [expr evaluateInContext:ctx];
-//        TDEqualObjects(obj, res);
-//    }
-//    
-//    TDNil([expr evaluateInContext:ctx]);
-//}
-//
-//- (void)testObjInSetOneTwoThree {
-//    std::string input = "i in foo";
-//    Reader reader(input);
-//    
-//    id foo = [NSSet setWithObjects:@"one", @"two", @"three", nil];
-//    id vars = @{@"foo": foo};
-//    TDTemplateContext *ctx = [[[TDTemplateContext alloc] initWithVariables:vars output:nil] autorelease];
-//    
-//    NSError *err = nil;
-//    TDExpression *expr = [[self.eng loopExpressionFromReader:&reader error:&err] simplify];
-//    TDNil(err);
-//    TDNotNil(expr);
-//    TDTrue([expr isKindOfClass:[TDLoopExpression class]]);
-//    
-//    NSMutableSet *test = [NSMutableSet setWithCapacity:[foo count]];
-//    for (id obj in foo) {
-//        TDNotNil(obj); // compiler warn
-//        id res = [expr evaluateInContext:ctx];
-//        TDNotNil(res);
-//        [test addObject:res];
-//    }
-//    
-//    TDNil([expr evaluateInContext:ctx]);
-//    
-//    for (id obj in foo) {
-//        TDTrue([test containsObject:obj]);
-//    }
-//}
-//
-//- (void)testKeyInDictOneTwoThree {
-//    std::string input = "key in dict";
-//    Reader reader(input);
-//    
-//    id dict = @{@"one": @(1), @"two": @(2), @"three": @(3)};
-//    id vars = @{@"dict": dict};
-//    TDTemplateContext *ctx = [[[TDTemplateContext alloc] initWithVariables:vars output:nil] autorelease];
-//    
-//    NSError *err = nil;
-//    TDExpression *expr = [[self.eng loopExpressionFromReader:&reader error:&err] simplify];
-//    TDNil(err);
-//    TDNotNil(expr);
-//    TDTrue([expr isKindOfClass:[TDLoopExpression class]]);
-//    
-//    for (id key in dict) {
-//        id res = [expr evaluateInContext:ctx];
-//        TDEqualObjects(key, res);
-//        TDEqualObjects(key, [ctx resolveVariable:@"key"]);
-//    }
-//    
-//    TDNil([expr evaluateInContext:ctx]);
-//}
-//
-//- (void)testKeyValInDictOneTwoThree {
-//    std::string input = "key,val in dict";
-//    Reader reader(input);
-//    
-//    id dict = @{@"one": @(1), @"two": @(2), @"three": @(3)};
-//    id vars = @{@"dict": dict};
-//    TDTemplateContext *ctx = [[[TDTemplateContext alloc] initWithVariables:vars output:nil] autorelease];
-//    
-//    NSError *err = nil;
-//    TDExpression *expr = [[self.eng loopExpressionFromReader:&reader error:&err] simplify];
-//    TDNil(err);
-//    TDNotNil(expr);
-//    TDTrue([expr isKindOfClass:[TDLoopExpression class]]);
-//    
-//    for (id key in [dict allKeys]) {
-//        id val = dict[key];
-//        id res = [expr evaluateInContext:ctx];
-//        TDEqualObjects((@[key, val]), res);
-//        TDEqualObjects(key, [ctx resolveVariable:@"key"]);
-//        TDEqualObjects(val, [ctx resolveVariable:@"val"]);
-//    }
-//    
-//    TDNil([expr evaluateInContext:ctx]);
-//}
-//
+- (void)testIInArrayOneTwoThree {
+    std::string input = "i in foo";
+    Reader reader(input);
+    
+    id foo = @[@"one", @"two", @"three"];
+    id vars = @{@"foo": foo};
+    TDTemplateContext *ctx = [[[TDTemplateContext alloc] initWithVariables:vars output:nil] autorelease];
+    
+    NSError *err = nil;
+    TDExpression *expr = [[self.eng loopExpressionFromReader:&reader error:&err] simplify];
+    TDNil(err);
+    TDNotNil(expr);
+    TDTrue([expr isKindOfClass:[TDLoopExpression class]]);
+    
+    for (id obj in foo) {
+        id res = [expr evaluateInContext:ctx];
+        TDEqualObjects(obj, res);
+    }
+    
+    TDNil([expr evaluateInContext:ctx]);
+}
+
+- (void)testObjInSetOneTwoThree {
+    std::string input = "i in foo";
+    Reader reader(input);
+    
+    id foo = [NSSet setWithObjects:@"one", @"two", @"three", nil];
+    id vars = @{@"foo": foo};
+    TDTemplateContext *ctx = [[[TDTemplateContext alloc] initWithVariables:vars output:nil] autorelease];
+    
+    NSError *err = nil;
+    TDExpression *expr = [[self.eng loopExpressionFromReader:&reader error:&err] simplify];
+    TDNil(err);
+    TDNotNil(expr);
+    TDTrue([expr isKindOfClass:[TDLoopExpression class]]);
+    
+    NSMutableSet *test = [NSMutableSet setWithCapacity:[foo count]];
+    for (id obj in foo) {
+        TDNotNil(obj); // compiler warn
+        id res = [expr evaluateInContext:ctx];
+        TDNotNil(res);
+        [test addObject:res];
+    }
+    
+    TDNil([expr evaluateInContext:ctx]);
+    
+    for (id obj in foo) {
+        TDTrue([test containsObject:obj]);
+    }
+}
+
+- (void)testKeyInDictOneTwoThree {
+    std::string input = "key in dict";
+    Reader reader(input);
+    
+    id dict = @{@"one": @(1), @"two": @(2), @"three": @(3)};
+    id vars = @{@"dict": dict};
+    TDTemplateContext *ctx = [[[TDTemplateContext alloc] initWithVariables:vars output:nil] autorelease];
+    
+    NSError *err = nil;
+    TDExpression *expr = [[self.eng loopExpressionFromReader:&reader error:&err] simplify];
+    TDNil(err);
+    TDNotNil(expr);
+    TDTrue([expr isKindOfClass:[TDLoopExpression class]]);
+    
+    for (id key in dict) {
+        id res = [expr evaluateInContext:ctx];
+        TDEqualObjects(key, res);
+        TDEqualObjects(key, [ctx resolveVariable:@"key"]);
+    }
+    
+    TDNil([expr evaluateInContext:ctx]);
+}
+
+- (void)testKeyValInDictOneTwoThree {
+    std::string input = "key,val in dict";
+    Reader reader(input);
+    
+    id dict = @{@"one": @(1), @"two": @(2), @"three": @(3)};
+    id vars = @{@"dict": dict};
+    TDTemplateContext *ctx = [[[TDTemplateContext alloc] initWithVariables:vars output:nil] autorelease];
+    
+    NSError *err = nil;
+    TDExpression *expr = [[self.eng loopExpressionFromReader:&reader error:&err] simplify];
+    TDNil(err);
+    TDNotNil(expr);
+    TDTrue([expr isKindOfClass:[TDLoopExpression class]]);
+    
+    for (id key in [dict allKeys]) {
+        id val = dict[key];
+        id res = [expr evaluateInContext:ctx];
+        TDEqualObjects((@[key, val]), res);
+        TDEqualObjects(key, [ctx resolveVariable:@"key"]);
+        TDEqualObjects(val, [ctx resolveVariable:@"val"]);
+    }
+    
+    TDNil([expr evaluateInContext:ctx]);
+}
+
 //- (void)testKeyValInDictNumsOneTwoThree {
 //    std::string input = "key,val in dict.nums";
 //    Reader reader(input);
