4e0cc601565a6b7ddf68a21f020d2fddd0a4397d
Todd Ditchendorf <todd.ditchendorf@gmail.com>
2025-05-27 09:41:57 -0500

    * modern, add filter

---
+++ src/TDAddFilter.m
@@ -0,0 +1,67 @@
+// The MIT License (MIT)
+//
+// Copyright (c) 2014 Todd Ditchendorf
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+#import <TDTemplateEngine/TDAddFilter.h>
+#import <TDTemplateEngine/TDExpression.h>
+
+@implementation TDAddFilter
+
++ (NSString *)filterName {
+    return @"add";
+}
+
+
+- (id)runFilter:(id)input withArgs:(NSArray *)args inContext:(TDTemplateContext *)ctx {
+    TDAssert(input);
+    
+    [self validateArgs:args min:1 max:1];
+
+    id res = nil;
+    id arg = [args objectAtIndex:0];
+    
+    if ([input respondsToSelector:@selector(integerValue)]) {
+        NSInteger i = [input integerValue];
+        NSInteger j = (NSInteger)[arg evaluateAsNumberInContext:ctx];
+        res = @(i + j);
+    } else if ([input isKindOfClass:[NSString class]]) {
+        NSString *rhs = [arg evaluateAsStringInContext:ctx];
+        res = [NSString stringWithFormat:@"%@%@", input, rhs];
+    } else if ([input isKindOfClass:[NSArray class]]) {
+        id rhs = [arg evaluateAsObjectInContext:ctx];
+        res = [NSMutableArray arrayWithArray:input];
+        if ([rhs isKindOfClass:[NSArray class]]) {
+            [res addObjectsFromArray:rhs];
+        } else {
+            [res addObject:rhs];
+        }
+    } else if ([input isKindOfClass:[NSDictionary class]]) {
+        id rhs = [arg evaluateAsObjectInContext:ctx];
+        if ([rhs isKindOfClass:[NSDictionary class]]) {
+            res = [NSMutableDictionary dictionaryWithDictionary:input];
+            [res addEntriesFromDictionary:rhs];
+        }
+    }
+    
+    return res;
+}
+
+@end
