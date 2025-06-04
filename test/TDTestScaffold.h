fdbe5a4b83980c0724f0848d3d9d5ab118f07328
Todd Ditchendorf <todd.ditchendorf@gmail.com>
2025-06-04 12:26:33 -0500

    * project folders cleanup

--- test/TDTestScaffold.h
+++
@@ -1,9 +0,0 @@
-//
-//  TDTestScaffold.h
-//  TDTemplateEngineTests
-//
-//  Created by Todd Ditchendorf on 3/28/14.
-//  Copyright (c) 2014 Todd Ditchendorf. All rights reserved.
-//
-
-#import <Foundation/Foundation.h>
